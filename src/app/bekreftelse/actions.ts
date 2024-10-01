d6379da3e091cbca76a17ceb5b6af77c6532ee4a
Jostein Holje <jostein.holje@nav.no>
2024-10-01 16:02:37 +0200

    optimize imports

--- src/app/bekreftelse/actions.ts
+++ src/app/bekreftelse/actions.ts
@@ -5,8 +5,6 @@ import { stripBearer } from '@navikt/oasis/dist/strip-bearer';
 import { logger } from '@navikt/next-logger';
 import { headers } from 'next/headers';
 import { v4 as uuidv4 } from 'uuid';
-import { IdportenPayload } from '@navikt/oasis/dist/validate';
-import { ParseResult } from '@navikt/oasis/dist/parse-token';
 
 const brukerMock = process.env.ENABLE_MOCK === 'enabled';
 
@@ -46,9 +44,9 @@ async function fetchTilgjengeligeBekreftelser(): Promise<{
     const tokenXToken = await getTokenXToken(idPortenToken);
     const TILGJENGELIGE_BEKREFTELSER_URL = `${process.env.BEKREFTELSE_API_URL}/api/v1/tilgjengelige-bekreftelser`;
     const traceId = uuidv4();
-    logger.info({'x_trace_id': traceId}, `Starter GET ${TILGJENGELIGE_BEKREFTELSER_URL}`);
+    logger.info({ x_trace_id: traceId }, `Starter GET ${TILGJENGELIGE_BEKREFTELSER_URL}`);
 
-    const parsedToken =  parseIdportenToken(idPortenToken)
+    const parsedToken = parseIdportenToken(idPortenToken);
     const identitetsnummer = parsedToken.ok ? parsedToken.pid : null;
 
     const response = await fetch(TILGJENGELIGE_BEKREFTELSER_URL, {
@@ -62,7 +60,10 @@ async function fetchTilgjengeligeBekreftelser(): Promise<{
         },
     });
 
-    logger.info({'x_trace_id': traceId}, `Ferdig GET ${TILGJENGELIGE_BEKREFTELSER_URL} ${response.status} ${response.statusText}`);
+    logger.info(
+        { x_trace_id: traceId },
+        `Ferdig GET ${TILGJENGELIGE_BEKREFTELSER_URL} ${response.status} ${response.statusText}`,
+    );
 
     if (!response.ok) {
         const error: any = new Error(`${response.status} ${response.statusText}`);
