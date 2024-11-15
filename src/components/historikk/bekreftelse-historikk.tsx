e1955287addf63e1562a5f9b49409a7a6cdbb18f
Geir Gåsodden <geir.gasodden@gmail.com>
2024-11-15 13:20:20 +0100

    r første utkast av bekreftelsehistorikk

---
+++ src/components/historikk/bekreftelse-historikk.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Bekreftelse } from "@navikt/arbeidssokerregisteret-utils";
+import { Heading, Table } from "@navikt/ds-react";
+
+import { prettyPrintDato } from "@/lib/date-utils";
+
+export function BekreftelseHistorikk (props: {bekreftelser: Bekreftelse[]}) {
+  const { bekreftelser } = props
+
+  if (bekreftelser.length === 0) return null
+
+  return (
+    <>
+    <Heading level="2" size="medium">
+      Arbeidsøkerperioden bekreftet
+    </Heading>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          {/* <Table.HeaderCell scope="col">Periode</Table.HeaderCell> */}
+          <Table.HeaderCell scope="col">Innsendt dato</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Innsendt av</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Jobbet i perioden</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Vil fortsatt være arbeidssøker</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+          {bekreftelser.map(({periodeId, svar}, i) => {
+            return (
+              <Table.Row key={i + periodeId}>
+                {/* <Table.HeaderCell scope="row">{prettyPrintDato(svar.gjelderFra)} - {prettyPrintDato(svar.gjelderTil)}</Table.HeaderCell> */}
+                <Table.DataCell>{prettyPrintDato(svar.sendtInnAv.tidspunkt)}</Table.DataCell>
+                <Table.DataCell>{svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
+                <Table.DataCell>{svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}</Table.DataCell>
+                <Table.DataCell>{svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}</Table.DataCell>
+              </Table.Row>
+            )
+          })}
+      </Table.Body>
+    </Table>
+    </>
+  )
+}
--- src/components/historikk/historikk-wrapper.tsx
+++ src/components/historikk/historikk-wrapper.tsx
@@ -1,6 +1,8 @@
 import { prettyPrintDato } from "@/lib/date-utils";
 import { ArbeidssokerPeriode, Bekreftelse, OpplysningerOmArbeidssoker, Profilering } from "@navikt/arbeidssokerregisteret-utils";
-import { Heading } from "@navikt/ds-react";
+import { Heading, BodyShort } from "@navikt/ds-react";
+
+import { BekreftelseHistorikk } from "./bekreftelse-historikk";
 
 export interface Historikk extends ArbeidssokerPeriode {
   bekreftelser: Bekreftelse[];
@@ -16,6 +18,16 @@ export function HistorikkWrapper (props: Historikk) {
       <Heading level="2" size="large">
         {prettyPrintDato(startet.tidspunkt)} - {avsluttet && avsluttet.tidspunkt ? prettyPrintDato(avsluttet.tidspunkt): 'fortsatt aktiv'}
       </Heading>
+      <BodyShort>
+        Startet: {prettyPrintDato(startet.tidspunkt)} av {startet.utfoertAv.type}
+      </BodyShort>
+      <BodyShort>
+        Avsluttet: {avsluttet && avsluttet.tidspunkt ? `${prettyPrintDato(avsluttet.tidspunkt)} av ${avsluttet.utfoertAv.type}` : 'fortsatt aktiv'} {}
+      </BodyShort>
+      <BodyShort>
+        Sluttårsak: {avsluttet && avsluttet.aarsak ? avsluttet.aarsak : 'fortsatt aktiv'}
+      </BodyShort>
+      <BekreftelseHistorikk bekreftelser={bekreftelser} />
       <pre>{JSON.stringify(props, null, 2)}</pre>
     </>
   )
