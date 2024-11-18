d863feb64aa45c9dd4772096eaa88c6ded295f31
Geir Gåsodden <geir.gasodden@gmail.com>
2024-11-18 12:13:28 +0100

    r forsøker å få med alle data for bekreftelse over to rader

--- src/components/historikk/bekreftelse-historikk.tsx
+++ src/components/historikk/bekreftelse-historikk.tsx
@@ -4,6 +4,7 @@ import { Bekreftelse } from "@navikt/arbeidssokerregisteret-utils";
 import { Heading, Table, ReadMore } from "@navikt/ds-react";
 
 import { prettyPrintDato } from "@/lib/date-utils";
+import { TableDataCell } from "@navikt/ds-react/Table";
 
 function AlleBekreftelser (props: {bekreftelser: Bekreftelse[]}) {
   const { bekreftelser } = props
@@ -20,28 +21,26 @@ function AlleBekreftelser (props: {bekreftelser: Bekreftelse[]}) {
         <Table.Row>
           <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
           <Table.HeaderCell scope="col">Innsendt dato</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Vis alt</Table.HeaderCell>
-          {/* 
           <Table.HeaderCell scope="col">Innsendt av</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Jobbet i perioden</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Vil fortsatt være arbeidssøker</Table.HeaderCell>
-          */}
-          
         </Table.Row>
       </Table.Header>
       <Table.Body>
           {bekreftelser.map(({periodeId, svar}, i) => {
             return (
+              <>
               <Table.Row key={i + periodeId}>
-                <Table.HeaderCell scope="row">{prettyPrintDato(svar.gjelderFra)} - {prettyPrintDato(svar.gjelderTil)}</Table.HeaderCell>
+                <Table.DataCell>{prettyPrintDato(svar.gjelderFra)} - {prettyPrintDato(svar.gjelderTil)}</Table.DataCell>
                 <Table.DataCell>{prettyPrintDato(svar.sendtInnAv.tidspunkt)}</Table.DataCell>
-                <Table.DataCell>Vis alt</Table.DataCell>
-                {/*
-                  <Table.DataCell>{svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
-                  <Table.DataCell>{svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}</Table.DataCell>
-                  <Table.DataCell>{svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}</Table.DataCell>
-                */}
+                <Table.DataCell>{svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
               </Table.Row>
+              <Table.Row>
+                <TableDataCell colSpan={3}>
+                Jobbet i perioden: {svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}
+                {' '}
+                Vil fortsatt være arbeidssøker: {svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}
+                </TableDataCell>
+              </Table.Row>
+              </>
             )
           })}
       </Table.Body>
@@ -67,28 +66,22 @@ export function BekreftelseHistorikk (props: {bekreftelser: Bekreftelse[]}) {
         <Table.Row>
           <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
           <Table.HeaderCell scope="col">Innsendt dato</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Vis alt</Table.HeaderCell>
-          {/* 
           <Table.HeaderCell scope="col">Innsendt av</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Jobbet i perioden</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Vil fortsatt være arbeidssøker</Table.HeaderCell>
-          */}
-          
         </Table.Row>
       </Table.Header>
       <Table.Body>
-          
               <Table.Row >
-                <Table.HeaderCell scope="row">{prettyPrintDato(sisteBekreftelse.svar.gjelderFra)} - {prettyPrintDato(sisteBekreftelse.svar.gjelderTil)}</Table.HeaderCell>
+                <Table.DataCell>{prettyPrintDato(sisteBekreftelse.svar.gjelderFra)} - {prettyPrintDato(sisteBekreftelse.svar.gjelderTil)}</Table.DataCell>
                 <Table.DataCell>{prettyPrintDato(sisteBekreftelse.svar.sendtInnAv.tidspunkt)}</Table.DataCell>
-                <Table.DataCell>Vis alt</Table.DataCell>
-                {/*
-                  <Table.DataCell>{svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
-                  <Table.DataCell>{svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}</Table.DataCell>
-                  <Table.DataCell>{svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}</Table.DataCell>
-                */}
+                <Table.DataCell>{sisteBekreftelse.svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
               </Table.Row>
-           
+              <Table.Row>
+                <TableDataCell colSpan={3}>
+                Jobbet i perioden: {sisteBekreftelse.svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}
+                {' '}
+                Vil fortsatt være arbeidssøker: {sisteBekreftelse.svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}
+                </TableDataCell>
+              </Table.Row>
       </Table.Body>
     </Table>
     <AlleBekreftelser bekreftelser={bekreftelser} />
