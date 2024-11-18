803cf821d4c067e4a1d1cd02b383eb7e6bcb35ee
Geir Gåsodden <geir.gasodden@gmail.com>
2024-11-18 10:42:23 +0100

    f ny component for opplysningr historikk

---
+++ src/components/historikk/opplysninger-historikk.tsx
@@ -0,0 +1,8 @@
+import { OpplysningerOmArbeidssoker } from "@navikt/arbeidssokerregisteret-utils";
+
+export function Opplysninger (props: { opplysningerOmArbeidssoker: OpplysningerOmArbeidssoker}) {
+  const { opplysningerOmArbeidssoker } = props
+
+  if (!opplysningerOmArbeidssoker) return null
+
+}
