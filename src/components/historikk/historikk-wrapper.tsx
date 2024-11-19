85b54a35a8244628418c9af80bf44e0344c2c52c
Jostein Holje <jostein.holje@nav.no>
2024-11-19 15:06:58 +0100

    Begynner med språkstøtte og tekst-mapping i historikk-wrapper

--- src/components/historikk/historikk-wrapper.tsx
+++ src/components/historikk/historikk-wrapper.tsx
@@ -1,40 +1,61 @@
-import { prettyPrintDato } from "@/lib/date-utils";
+import { prettyPrintDato } from '@/lib/date-utils';
 import {
     ArbeidssokerPeriode,
     Bekreftelse,
+    lagHentTekstForSprak,
     OpplysningerOmArbeidssoker,
     Profilering,
-    Sprak
+    Sprak,
 } from '@navikt/arbeidssokerregisteret-utils';
-import { AggregertPeriode } from "../../../types/aggregerte-perioder";
-import { Heading, BodyShort } from "@navikt/ds-react";
+import { AggregertPeriode } from '../../../types/aggregerte-perioder';
+import { Heading, BodyShort } from '@navikt/ds-react';
 
-import { BekreftelseHistorikk } from "./bekreftelse-historikk";
+import { BekreftelseHistorikk } from './bekreftelse-historikk';
 import { OpplysningerHistorikk } from '@/components/historikk/opplysninger-historikk';
 
 export interface Historikk extends AggregertPeriode {
-  sprak: Sprak;
+    sprak: Sprak;
 }
 
-export function HistorikkWrapper (props: Historikk) {
-  const {startet, avsluttet, bekreftelser, opplysningerOmArbeidssoeker, sprak} = props
-
-  return (
-    <>
-      <Heading level="2" size="large">
-        {prettyPrintDato(startet.tidspunkt)} - {avsluttet && avsluttet.tidspunkt ? prettyPrintDato(avsluttet.tidspunkt): 'fortsatt aktiv'}
-      </Heading>
-      <BodyShort>
-        Startet: {prettyPrintDato(startet.tidspunkt)} av {startet.utfoertAv.type}
-      </BodyShort>
-      <BodyShort>
-        Avsluttet: {avsluttet && avsluttet.tidspunkt ? `${prettyPrintDato(avsluttet.tidspunkt)} av ${avsluttet.utfoertAv.type}` : 'fortsatt aktiv'} {}
-      </BodyShort>
-      <BodyShort>
-        Sluttårsak: {avsluttet && avsluttet.aarsak ? avsluttet.aarsak : 'fortsatt aktiv'}
-      </BodyShort>
-      <BekreftelseHistorikk bekreftelser={bekreftelser} />
-      <OpplysningerHistorikk opplysningerOmArbeidssoker={opplysningerOmArbeidssoeker} sprak={sprak} className={'mt-4'}/>
-    </>
-  )
+const TEKSTER = {
+    nb: {
+        startet: 'Startet',
+        avsluttet: 'Avsluttet',
+        sluttarsak: 'Sluttårsak',
+        av: 'av',
+        SLUTTBRUKER: 'bruker',
+        SYSTEM: 'Nav',
+        VEILEDER: 'veileder',
+        'fortsatt aktiv': 'fortsatt aktiv',
+        'graceperiode utløpt': 'Ikke bekreftet arbeidssøkerstatus',
+        'stopp av periode': 'Stoppet av veileder',
+    },
+};
+export function HistorikkWrapper(props: Historikk) {
+    const { startet, avsluttet, bekreftelser, opplysningerOmArbeidssoeker, sprak } = props;
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    return (
+        <>
+            <Heading level="2" size="large">
+                {prettyPrintDato(startet.tidspunkt)} -{' '}
+                {avsluttet && avsluttet.tidspunkt ? prettyPrintDato(avsluttet.tidspunkt) : tekst('fortsatt aktiv')}
+            </Heading>
+            <BodyShort>
+                {tekst('startet')}: {prettyPrintDato(startet.tidspunkt)} {tekst('av')} {tekst(startet.utfoertAv.type)}
+            </BodyShort>
+            <BodyShort>
+                {tekst('avsluttet')}:{' '}
+                {avsluttet && avsluttet.tidspunkt
+                    ? <>{prettyPrintDato(avsluttet.tidspunkt)} {tekst('av')} {tekst(avsluttet.utfoertAv.type)}</>
+                    : tekst('fortsatt aktiv')}
+            </BodyShort>
+            <BodyShort>{tekst('sluttarsak')}: {tekst(avsluttet?.aarsak.toLocaleLowerCase() ?? 'fortsatt aktiv')}</BodyShort>
+            <BekreftelseHistorikk bekreftelser={bekreftelser} />
+            <OpplysningerHistorikk
+                opplysningerOmArbeidssoker={opplysningerOmArbeidssoeker}
+                sprak={sprak}
+                className={'mt-4'}
+            />
+        </>
+    );
 }
