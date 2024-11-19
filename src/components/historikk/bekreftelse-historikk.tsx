5e2ccbc821b8cbaae7aa6fd64abc0daee41e4497
Geir Gåsodden <geir.gasodden@gmail.com>
2024-11-19 07:08:07 +0100

    r fjerner tabell også fra siste bekreftelse

--- src/components/historikk/bekreftelse-historikk.tsx
+++ src/components/historikk/bekreftelse-historikk.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { Bekreftelse } from "@navikt/arbeidssokerregisteret-utils";
-import { Heading, Table, ReadMore, Box, BodyShort } from "@navikt/ds-react";
+import { Heading, ReadMore, Box, BodyShort } from "@navikt/ds-react";
 
 import { prettyPrintDato } from "@/lib/date-utils";
-import { TableDataCell } from "@navikt/ds-react/Table";
 
 function AlleBekreftelser (props: {bekreftelser: Bekreftelse[]}) {
   const { bekreftelser } = props
@@ -18,7 +17,7 @@ function AlleBekreftelser (props: {bekreftelser: Bekreftelse[]}) {
     <ReadMore header="Se alle bekreftelser fra arbeidssøkerperioden">
       {bekreftelser.map(({periodeId, svar}, index) => {
             return (
-      <Box key={index + periodeId} className="mb-3" style={{ background: index % 2 !== 0 ? 'var(--a-surface-subtle)' : undefined }}>
+      <Box key={index + periodeId} className="p-4" style={{ background: index % 2 !== 0 ? 'var(--a-surface-subtle)' : undefined }}>
         <Heading size="small" level="3">
           {prettyPrintDato(svar.gjelderFra)} - {prettyPrintDato(svar.gjelderTil)}
         </Heading>
@@ -45,33 +44,24 @@ export function BekreftelseHistorikk (props: {bekreftelser: Bekreftelse[]}) {
 
   return (
     <>
-    <Heading level="2" size="medium">
-      Arbeidsøkerperioden bekreftet
-    </Heading>
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Innsendt dato</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Innsendt av</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-              <Table.Row >
-                <Table.DataCell>{prettyPrintDato(sisteBekreftelse.svar.gjelderFra)} - {prettyPrintDato(sisteBekreftelse.svar.gjelderTil)}</Table.DataCell>
-                <Table.DataCell>{prettyPrintDato(sisteBekreftelse.svar.sendtInnAv.tidspunkt)}</Table.DataCell>
-                <Table.DataCell>{sisteBekreftelse.svar.sendtInnAv.utfoertAv.type}</Table.DataCell>
-              </Table.Row>
-              <Table.Row>
-                <TableDataCell colSpan={3}>
-                Jobbet i perioden: {sisteBekreftelse.svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}
-                {' '}
-                Vil fortsatt være arbeidssøker: {sisteBekreftelse.svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}
-                </TableDataCell>
-              </Table.Row>
-      </Table.Body>
-    </Table>
-    <AlleBekreftelser bekreftelser={bekreftelser} />
+      <Heading level="2" size="medium">
+        Arbeidsøkerperioden bekreftet
+      </Heading>
+      <Box className="p-4">
+          <Heading size="small" level="3">
+            {prettyPrintDato(sisteBekreftelse.svar.gjelderFra)} - {prettyPrintDato(sisteBekreftelse.svar.gjelderTil)}
+          </Heading>
+          <BodyShort>
+            Innsendt {prettyPrintDato(sisteBekreftelse.svar.sendtInnAv.tidspunkt)} av {sisteBekreftelse.svar.sendtInnAv.utfoertAv.type}
+          </BodyShort>
+          <BodyShort>
+          Jobbet i perioden: {sisteBekreftelse.svar.harJobbetIDennePerioden ? 'Ja' : 'Nei'}
+          </BodyShort>
+          <BodyShort>
+          Vil fortsatt være arbeidssøker: {sisteBekreftelse.svar.vilFortsetteSomArbeidssoeker ? 'Ja' : 'Nei'}
+          </BodyShort>
+      </Box>
+      <AlleBekreftelser bekreftelser={bekreftelser} />
     </>
   )
 }
