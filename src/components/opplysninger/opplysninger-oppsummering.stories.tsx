9a4671b301d69a0c1056cb69dd88dcd700b51379
Jostein Holje <jostein.holje@nav.no>
2024-12-02 15:11:05 +0100

    Oppdater props i story

--- src/components/opplysninger/opplysninger-oppsummering.stories.tsx
+++ src/components/opplysninger/opplysninger-oppsummering.stories.tsx
@@ -1,9 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { behovsvurderingMockData, samletInformasjonMockData } from '@/app/mockdata';
+import { samletInformasjonMockData } from '@/app/mockdata';
 import { OpplysningerOppsummering } from '@/components/opplysninger/opplysninger-oppsummering';
 
 const opplysningerOmArbeidssoker = samletInformasjonMockData.opplysningerOmArbeidssoeker;
-const arbeidssokerperioderMock = samletInformasjonMockData.arbeidssoekerperioder;
 
 const meta = {
     title: 'Komponenter/OpplysningerOppsummering',
@@ -20,8 +19,6 @@ export const OpplysningerOmArbeidssoker: Story = {
     args: {
         opplysninger: opplysningerOmArbeidssoker[0] as any,
         sprak: 'nb',
-        behovsvurdering: behovsvurderingMockData,
-        harAktivPeriode: arbeidssokerperioderMock[0].avsluttet === null,
-        oppdaterOpplysningerUrl: process.env.OPPDATER_OPPLYSNINGER_URL!
+        oppdaterOpplysningerUrl: process.env.OPPDATER_OPPLYSNINGER_URL!,
     },
 };
