79dc46d34cac1f338860c25b7ce09d6ec6f2ad0f
Jostein Holje <jostein.holje@nav.no>
2024-12-02 13:36:34 +0100

    Send med riktig props

--- src/components/opplysninger/mangler-opplysninger.stories.tsx
+++ src/components/opplysninger/mangler-opplysninger.stories.tsx
@@ -1,19 +1,20 @@
-import {Meta, StoryObj} from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import ManglerOpplysninger from './mangler-opplysninger';
 
 const meta = {
-  title: "Komponenter/Mangler Opplysninger",
-  component: ManglerOpplysninger,
-  decorators: [],
-  tags: ["autodocs"],
-  args: {}
+    title: 'Komponenter/Mangler Opplysninger',
+    component: ManglerOpplysninger,
+    decorators: [],
+    tags: ['autodocs'],
+    args: {},
 } satisfies Meta<typeof ManglerOpplysninger>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const ManglerOpplysningerKomponent: Story = {
-  args: {
-    sprak: 'nb'
-  }
-}
+    args: {
+        sprak: 'nb',
+        oppdaterOpplysningerUrl: process.env.OPPDATER_OPPLYSNINGER_URL!,
+    },
+};
