70859f9469999053208429e55ea433b06906311a
Jostein Holje <jostein.holje@nav.no>
2024-12-04 13:59:29 +0100

    Språk-støttende historikklenke

--- src/components/historikk/se-historikk-lenke.tsx
+++ src/components/historikk/se-historikk-lenke.tsx
@@ -1,39 +1,42 @@
-'use client'
+'use client';
 
-import { Box, Link } from "@navikt/ds-react";
+import { Box, Link } from '@navikt/ds-react';
 import { lagHentTekstForSprak, Sprak } from '@navikt/arbeidssokerregisteret-utils';
 
-import { loggAktivitet } from "@/lib/amplitude";
+import { loggAktivitet } from '@/lib/amplitude';
+import tilSprakAvhengigAppPath from '@/lib/sprak-avhengig-url';
 
 export interface HistorikkLenkeProps {
-  sprak: Sprak;
+    sprak: Sprak;
 }
 
 const TEKSTER = {
-  nb: {
-      lenkeTekst: 'Se tidligere registreringer og opplysninger',
-  },
-  nn: {
-    lenkeTekst: 'Se tidligere registreringer og opplysninger',
-  },
-  en: {
-    lenkeTekst: 'Se tidligere registreringer og opplysninger',
-}
+    nb: {
+        lenkeTekst: 'Se tidligere registreringer og opplysninger',
+    },
+    nn: {
+        lenkeTekst: 'Se tidligere registreringer og opplysninger',
+    },
+    en: {
+        lenkeTekst: 'Se tidligere registreringer og opplysninger',
+    },
 };
 
 async function loggKlikk() {
-  loggAktivitet({aktivitet: 'Går til siden for historikk'})
-  console.log('du klikka mæ')
+    loggAktivitet({ aktivitet: 'Går til siden for historikk' });
+    console.log('du klikka mæ');
 }
 
-export function SeHistorikkLenke (props: HistorikkLenkeProps) {
-  const { sprak } = props;
+export function SeHistorikkLenke(props: HistorikkLenkeProps) {
+    const { sprak } = props;
 
-  const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
 
-  return (
-    <Box className="text-right mb-8">
-      <Link href="/arbeidssoekerregisteret/historikk" onClick={loggKlikk}>{tekst('lenkeTekst')}</Link>
-    </Box>
-  )
+    return (
+        <Box className="text-right mb-8">
+            <Link href={`/arbeidssoekerregisteret${tilSprakAvhengigAppPath('/historikk', sprak)}`} onClick={loggKlikk}>
+                {tekst('lenkeTekst')}
+            </Link>
+        </Box>
+    );
 }
