9c200d9600363505f194fa31ca759ea961668211
Jostein Holje <jostein.holje@nav.no>
2024-12-05 10:18:14 +0100

    Språkstøtte på opplysninger

--- src/components/opplysninger/opplysninger-oppsummering.tsx
+++ src/components/opplysninger/opplysninger-oppsummering.tsx
@@ -16,27 +16,44 @@ type Props = {
     oppdaterOpplysningerUrl: string;
 };
 
+const TEKSTER = {
+    nb: {
+        heading: 'Opplysninger fra registrering',
+        linkText: 'Endre svar',
+    },
+    nn: {
+        heading: 'Opplysningar frå registrering',
+        linkText: 'Endre svar',
+    },
+    en: {
+        heading: 'Answers from registration',
+        linkText: 'Edit',
+    },
+};
+
 const OpplysningerOppsummering = (props: Props) => {
     const { opplysninger, sprak, oppdaterOpplysningerUrl } = props;
     const besvarelser = mapOpplysninger(opplysninger, sprak);
-    const tekst = lagHentTekstForSprak(SPORSMAL_TEKSTER, props.sprak);
-
+    const besvarelseTekst = lagHentTekstForSprak(SPORSMAL_TEKSTER, sprak);
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
     return (
         <FormSummary>
             <FormSummary.Header>
-                <FormSummary.Heading level="2">Opplysninger fra registrering</FormSummary.Heading>
+                <FormSummary.Heading level="2">{tekst('heading')}</FormSummary.Heading>
                 <FormSummary.EditLink
                     href={oppdaterOpplysningerUrl}
                     onClick={() => loggAktivitet({ aktivitet: 'Trykker på "Endre opplysninger"' })}
-                />
+                >
+                    {tekst('linkText')}
+                </FormSummary.EditLink>
             </FormSummary.Header>
             <FormSummary.Answers>
                 {besvarelser.map((besvarelse, idx) => {
                     const { sporsmal, svar } = besvarelse;
                     return (
                         <FormSummary.Answer key={`${sporsmal}-${idx}`}>
-                            <FormSummary.Label>{tekst(sporsmal)}</FormSummary.Label>
-                            <FormSummary.Value>{tekst(svar as string) ?? svar}</FormSummary.Value>
+                            <FormSummary.Label>{besvarelseTekst(sporsmal)}</FormSummary.Label>
+                            <FormSummary.Value>{besvarelseTekst(svar as string) ?? svar}</FormSummary.Value>
                         </FormSummary.Answer>
                     );
                 })}
