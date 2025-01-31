32c5d3b95b7b05ce7587ad92417a8b88fca4f3a2
Jostein Holje <jostein.holje@nav.no>
2025-01-31 12:40:45 +0100

    Funksjon og test for å hente ut innloggingsnivå

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import SettSprakIDekorator from '@/components/sett-sprak-i-dekorator';
 import { SeHistorikkLenke } from '@/components/historikk/se-historikk-lenke';
 import ManglerOpplysninger from '@/components/opplysninger/mangler-opplysninger';
 import Feil from '@/components/feil';
+import { hentInnloggingsNivaa } from '@/lib/hent-innloggings-nivaa';
 
 interface Props {
     sprak: Sprak;
@@ -24,6 +25,9 @@ async function SamletInformasjonServerComponent({ sprak }: Props) {
     const { data: sisteSamletInformasjon, error: errorSisteSamletInformasjon } = await fetchSamletInformasjon({
         visKunSisteInformasjon: true,
     });
+
+    const { data: innloggingsNivaa } = await hentInnloggingsNivaa();
+
     const opplysninger = sisteSamletInformasjon?.opplysningerOmArbeidssoeker[0];
     const harAktivPeriode = sisteSamletInformasjon?.arbeidssoekerperioder[0]?.avsluttet === null;
     const harHistorikk = sisteSamletInformasjon?.arbeidssoekerperioder.length as any > 0;
@@ -43,6 +47,7 @@ async function SamletInformasjonServerComponent({ sprak }: Props) {
             <Suspense fallback={<Loader />}>
                 <TilgjengeligBekreftelseKomponent sprak={sprak} />
             </Suspense>
+            <div><h3>TEST Innloggingsnivå: {innloggingsNivaa}</h3></div>
             {harAktivPeriode && opplysninger && (
                 <div className={'my-6'}>
                     <OpplysningerOppsummering
