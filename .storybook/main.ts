5fb58d662ea926c23de7abc66450e517d930bab1
Jostein Holje <jostein.holje@nav.no>
2025-06-20 15:43:25 +0200

    Fix build-storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-    staticDirs: ['../public'],
+    staticDirs: ['../msw'],
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
         '@storybook/addon-onboarding',
