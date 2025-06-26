e01b634ae695acb1cfefba3299ca6645d180337e
Jostein Holje <jostein.holje@nav.no>
2025-06-26 14:23:29 +0200

    Vis egenvurdering med opplysninger

--- src/components/opplysninger/opplysninger-oppsummering.tsx
+++ src/components/opplysninger/opplysninger-oppsummering.tsx
@@ -9,9 +9,10 @@ import {
 import { FormSummary } from '@navikt/ds-react';
 import { mapOpplysninger } from '@/components/opplysninger/opplysninger';
 import { loggAktivitet } from '@/lib/amplitude';
+import { OpplysningerMedProfilering } from '../../../types/aggregerte-perioder';
 
 type Props = {
-    opplysninger: OpplysningerOmArbeidssoker;
+    opplysninger: OpplysningerMedProfilering;
     sprak: Sprak;
     oppdaterOpplysningerUrl: string;
     visEndreLink: boolean;
@@ -21,6 +22,9 @@ const TEKSTER = {
     nb: {
         heading: 'Opplysninger fra registrering',
         linkText: 'Endre svar',
+        egenvurderingLabel: 'Ønsker du veiledning fra Nav?',
+        ANTATT_GODE_MULIGHETER: 'Ønsker å klare meg selv',
+        ANTATT_BEHOV_FOR_VEILEDNING: 'Ønsker hjelp fra en veileder',
     },
     nn: {
         heading: 'Opplysningar frå registrering',
@@ -37,6 +41,7 @@ const OpplysningerOppsummering = (props: Props) => {
     const besvarelser = mapOpplysninger(opplysninger, sprak);
     const besvarelseTekst = lagHentTekstForSprak(SPORSMAL_TEKSTER, sprak);
     const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    const harEgenvurdering = Boolean(opplysninger?.profilering?.egenvurdering);
     return (
         <FormSummary>
             <FormSummary.Header>
@@ -60,6 +65,14 @@ const OpplysningerOppsummering = (props: Props) => {
                         </FormSummary.Answer>
                     );
                 })}
+                {harEgenvurdering && (
+                    <FormSummary.Answer>
+                        <FormSummary.Label>{tekst('egenvurderingLabel')}</FormSummary.Label>
+                        <FormSummary.Value>
+                            {tekst(opplysninger.profilering.egenvurdering?.egenvurdering as string)}
+                        </FormSummary.Value>
+                    </FormSummary.Answer>
+                )}
             </FormSummary.Answers>
         </FormSummary>
     );
--- types/aggregerte-perioder.ts
+++ types/aggregerte-perioder.ts
@@ -1,14 +1,31 @@
-import { ArbeidssokerPeriode, OpplysningerOmArbeidssoker, Bekreftelse, Profilering } from "@navikt/arbeidssokerregisteret-utils";
+import {
+    ArbeidssokerPeriode,
+    Bekreftelse,
+    OpplysningerOmArbeidssoker,
+    Profilering,
+    SendtInnAv,
+} from '@navikt/arbeidssokerregisteret-utils';
 
-import { BekreftelseMedStatus } from "@/model/bekreftelse";
+import { BekreftelseMedStatus } from '@/model/bekreftelse';
+import { ProfilertTil } from '@navikt/arbeidssokerregisteret-utils/dist/models/profilering';
 
-interface OpplysningerMedProfilering extends OpplysningerOmArbeidssoker {
-  profilering: Profilering
+export interface ProfileringMedEgenvurdering extends Profilering {
+    egenvurdering?: {
+        egenvurderingId: string;
+        periodeId: string;
+        opplysningerOmArbeidssoekerId: string;
+        sendtInnAv: SendtInnAv;
+        egenvurdering: ProfilertTil;
+    };
+}
+
+export interface OpplysningerMedProfilering extends OpplysningerOmArbeidssoker {
+    profilering: ProfileringMedEgenvurdering;
 }
 
 export interface AggregertPeriode extends ArbeidssokerPeriode {
-  opplysningerOmArbeidssoeker: OpplysningerMedProfilering[];
-  bekreftelser: Bekreftelse[]
+    opplysningerOmArbeidssoeker: OpplysningerMedProfilering[];
+    bekreftelser: Bekreftelse[];
 }
 
 export interface AggregertPeriodeMedBekreftelseStatus {
@@ -16,6 +33,6 @@ export interface AggregertPeriodeMedBekreftelseStatus {
     bekreftelser: BekreftelseMedStatus[];
 }
 
-export type AggregertePerioder = AggregertPeriode[]
+export type AggregertePerioder = AggregertPeriode[];
 
 export type AggregertePerioderMedBekreftelseStatus = AggregertePerioderMedBekreftelseStatus[];
