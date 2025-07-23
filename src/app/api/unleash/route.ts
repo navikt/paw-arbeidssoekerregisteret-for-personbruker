6e0f94cd6de8d26932b96c542a75fc3edfc5c15e
Jostein Holje <jostein.holje@nav.no>
2025-07-23 10:58:10 +0200

    Legger til tester for unleash api

--- src/app/api/unleash/route.ts
+++ src/app/api/unleash/route.ts
@@ -2,7 +2,9 @@ import { isEnabled } from '@/lib/unleash-is-enabled';
 import { NextRequest } from 'next/server';
 import { HttpResponse } from 'msw';
 
-const brukerMock = process.env.ENABLE_MOCK === 'enabled';
+function brukerMock() {
+    return process.env.ENABLE_MOCK === 'enabled';
+}
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
@@ -12,19 +14,14 @@ export async function GET(request: NextRequest) {
         return new HttpResponse(null, { status: 400 });
     }
 
-    if (brukerMock) {
+    if (brukerMock()) {
         const mockResponse = toggles.reduce((acc, key) => {
             return { ...acc, [key]: true };
         }, {});
         return HttpResponse.json(mockResponse);
     }
 
-    const response = toggles.reduce(async (acc, key) => {
-        return {
-            ...acc,
-            [key]: await isEnabled(key),
-        };
-    }, {});
-
+    const results = await Promise.all(toggles.map(async (key) => [key, await isEnabled(key)]));
+    const response = Object.fromEntries(results);
     return HttpResponse.json(response);
 }
