b7228b9dd077053d4df9b395a81f4a2ee4e126cd
Jostein Holje <jostein.holje@nav.no>
2025-10-10 10:46:48 +0200

    Slett ubrukte <InitSkyraSurvey>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,6 @@ async function SamletInformasjonServerComponent({ sprak }: Props) {
                 />
             )}
             {harHistorikk && <SeHistorikkLenke sprak={sprak} />}
-            {/*<InitSkyraSurvey aggregerteData={aggregerteData!} />*/}
         </>
     );
 }
