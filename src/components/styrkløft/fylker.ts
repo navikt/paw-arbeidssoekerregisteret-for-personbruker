a164a5132ccce20b1e97a3bee37dd57c8721198e
Jostein Holje <jostein.holje@nav.no>
2025-10-15 13:29:43 +0200

    Filter-data

---
+++ src/components/styrkløft/fylker.ts
@@ -0,0 +1,39 @@
+export const filterKey = 'county';
+
+export const fylker = [
+    'Agder',
+    'Akershus',
+    'Buskerud',
+    'Finnmark',
+    'Innlandet',
+    'Møre og Romsdal',
+    'Nordland',
+    'Oslo',
+    'Rogaland',
+    'Svalbard',
+    'Telemark',
+    'Troms',
+    'Trøndelag',
+    'Vestfold',
+    'Vestland',
+    'Østfold',
+];
+
+export const fylkeTilCounty = {
+    Agder: 'AGDER',
+    Akershus: 'AKERSHUS',
+    Buskerud: 'BUSKERUD',
+    Finnmark: 'FINNMARK',
+    Innlandet: 'INNANDET',
+    'Møre og Romsdal': 'MØRE+OG+ROMSDAL',
+    Nordland: 'NORDLAND',
+    Oslo: 'OSLO',
+    Rogaland: 'ROGALAND',
+    Svalbard: 'SVALBARD',
+    Telemark: 'TELEMARK',
+    Troms: 'TROMS',
+    Trøndelag: 'TRØNDELAG',
+    Vestfold: 'VESTFOLD',
+    Vestland: 'VESTLAND',
+    Østfold: 'ØSTFOLD',
+};
---
+++ src/components/styrkløft/yrkeskategorier.ts
@@ -0,0 +1,35 @@
+export const yrkeskategorier = [
+    'Bygg og anlegg',
+    'Helse og sosial',
+    'Håndverkere',
+    'Industri og produksjon',
+    'IT',
+    'Kontor og økonomi',
+    'Kultur og kreative yrker',
+    'Natur og miljø',
+    'Reiseliv og mat',
+    'Salg og service',
+    'Sikkerhet og beredskap',
+    'Transport og lager',
+    'Utdanning',
+    'Ikke oppgitt',
+];
+
+export const filterKey = 'occupationLevel1';
+
+export const yrkeskategorierTilOccupationLevel1 = {
+    'Bygg og anlegg': 'Bygg+og+anlegg',
+    'Helse og sosial': 'Helse+og+sosial',
+    'Håndverkere': 'Håndverkere',
+    'Industri og produksjon': 'Industri+og+produksjon',
+    'IT': 'IT',
+    'Kontor og økonomi': 'Kontor+og+økonomi',
+    'Kultur og kreative yrker': 'Kultur+og+kreative+yrker',
+    'Natur og miljø': 'Natur+og+miljø',
+    'Reiseliv og mat': 'Reiseliv+og+mat',
+    'Salg og service': 'Salg+og+service',
+    'Sikkerhet og beredskap': 'Sikkerhet+og+beredskap',
+    'Transport og lager': 'Transport+og+lager',
+    'Utdanning': 'Utdanning',
+    'Ikke oppgitt': 'Uoppgitt%2F+ikke+identifiserbare',
+}
