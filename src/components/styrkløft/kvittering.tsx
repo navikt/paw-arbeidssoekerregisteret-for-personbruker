73ac84c059cdd081f6fd1676478b870b3a263f8f
Jostein Holje <jostein.holje@nav.no>
2025-10-16 13:44:29 +0200

    Prøver <FilterVelger3> i <Kvittering>

--- src/components/styrkløft/kvittering.tsx
+++ src/components/styrkløft/kvittering.tsx
@@ -1,8 +1,7 @@
 import { Box, Button, Heading } from '@navikt/ds-react';
-import FilterVelger from '@/components/styrkløft/filter-velger';
 import { yrkeskategorier } from '@/components/styrkløft/yrkeskategorier';
 import { fylker } from '@/components/styrkløft/fylker';
-import FilterVelger2 from '@/components/styrkløft/filter-velger2';
+import FilterVelger3 from '@/components/styrkløft/filter-velger3';
 
 interface Props {}
 
@@ -13,16 +12,16 @@ function Kvittering(props: Props) {
                 Velg yrkeskategori og fylke du vil se stillinger fra
             </Heading>
             <section className={'my-4'}>
-                <FilterVelger
-                    tekst={'Velg yrkeskategori'}
+                <FilterVelger3
+                    values={['IT', 'Bygg og anlegg']}
                     options={yrkeskategorier}
                     heading={'Velg yrkeskategori'}
                     onChange={(val) => console.log('yrkeskategori valgt', val)}
                 />
             </section>
             <section className={'my-4'}>
-                <FilterVelger2
-                    tekst={'Velg fylke'}
+                <FilterVelger3
+                    values={['Østfold', 'Oslo']}
                     options={fylker}
                     heading={'Velg fylke'}
                     onChange={(val) => console.log('fylke valgt', val)}
