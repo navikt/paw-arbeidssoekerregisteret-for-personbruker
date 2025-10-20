8a757f154815ebd6fae53cf92a169d1b189e5e71
Jostein Holje <jostein.holje@nav.no>
2025-10-20 14:08:35 +0200

    hentYrkeskategorier for gitte styrk-koder
    + fiks alfabetisk sortering

--- src/lib/hent-yrkeskategorier.test.ts
+++ src/lib/hent-yrkeskategorier.test.ts
@@ -1,13 +1,13 @@
-import { hentYrkeskategorier } from '@/lib/hent-yrkeskategorier';
+import { hentAlleYrkeskategorier, hentYrkeskategorier } from '@/lib/hent-yrkeskategorier';
 
-describe('hent yrkeskategorier', () => {
+describe('hentAlleYrkeskategorier', () => {
     it('returnerer kategorie liste', () => {
-        expect(hentYrkeskategorier()).toEqual([
+        expect(hentAlleYrkeskategorier()).toEqual([
             'Bygg og anlegg',
             'Helse og sosial',
             'Håndverkere',
-            'IT',
             'Industri og produksjon',
+            'IT',
             'Kontor og økonomi',
             'Kultur og kreative yrker',
             'Natur og miljø',
@@ -16,6 +16,12 @@ describe('hent yrkeskategorier', () => {
             'Sikkerhet og beredskap',
             'Transport og lager',
             'Utdanning',
-        ])
-    })
-})
+        ]);
+    });
+});
+
+describe('hentYrkeskategorier', () => {
+    it('returnerer kategorie liste for gitte styrk-koder', () => {
+        expect(hentYrkeskategorier(['1330', '2166', '2514', '3514'])).toEqual(['IT']);
+    });
+});
--- src/lib/hent-yrkeskategorier.ts
+++ src/lib/hent-yrkeskategorier.ts
@@ -1,7 +1,31 @@
 import arbeidsplassenMapping from './arbeidsplassen-mapping.json';
 
-export function hentYrkeskategorier() {
+const alfabetiskSortering = (a: string, b: string) => {
+    const nameA = a.toLowerCase();
+    const nameB = b.toLowerCase();
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+    return 0;
+};
+
+export function hentAlleYrkeskategorier(): string[] {
     return Array.from(new Set(Object.values(arbeidsplassenMapping).map((d) => d.categoryLevel1)))
         .filter((c) => !c.includes('Uoppgitt'))
-        .sort();
+        .sort(alfabetiskSortering);
+}
+
+export function hentYrkeskategorier(styrkkoder: string[]) {
+    return Array.from(
+        new Set(
+            styrkkoder.map((styrk) => {
+                return arbeidsplassenMapping[styrk as keyof typeof arbeidsplassenMapping]?.categoryLevel1;
+            }),
+        ),
+    )
+        .filter((i) => i)
+        .sort(alfabetiskSortering);
 }
