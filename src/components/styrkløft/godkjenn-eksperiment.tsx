fa03799cab1522680e6c7171c22700e1e6cabf7f
Jostein Holje <jostein.holje@nav.no>
2025-10-20 14:58:19 +0200

    Fjern checkbox fra <GodkjennEksperiment>

--- src/components/styrkløft/godkjenn-eksperiment.tsx
+++ src/components/styrkløft/godkjenn-eksperiment.tsx
@@ -6,8 +6,6 @@ const TEKSTER = {
     nb: {
         heading: 'Har du lyst til å se ledige stillinger som kan passe for deg?',
         body: 'Hvis du sier ja til å dele informasjon med oss kan vi vise deg ledige stillinger i ditt område som kan være aktuelle ut fra den kompetansen du har',
-        checkboxText:
-            'Ja, Nav kan bruke informasjon de allerede har om meg til å vise stillinger som kan være aktuelle for meg',
         submitButton: 'Vis meg ledige stillinger',
         cancelButton: 'Nei takk, jeg ønsker ikke å se ledige stillinger',
     },
@@ -27,7 +25,6 @@ function GodkjennEksperiment(props: Props) {
                 <div className={'float-right'}><Beta sprak={sprak}/></div>
             </Heading>
             <BodyShort className={'py-4'}>{tekst('body')}</BodyShort>
-            <Checkbox value={'ok'}>{tekst('checkboxText')}</Checkbox>
             <div className={'flex flex-col gap-4 py-4'}>
                 <Button variant={'primary'}>{tekst('submitButton')}</Button>
                 <Button variant={'tertiary'}>{tekst('cancelButton')}</Button>
