3db382c6a1ac899bcc332419455c4f365fc5d1cd
Christian Rennemo <christian.rennemo@nav.no>
2025-10-23 12:00:11 +0200

    lagt til liste over alle fylker (mangler kommune pr nå)

--- src/components/styrkløft/fylker.ts
+++ src/components/styrkløft/fylker.ts
@@ -37,3 +37,66 @@ export const fylkeTilCounty = {
     Vestland: 'VESTLAND',
     Østfold: 'ØSTFOLD',
 };
+
+export const FYLKER_OG_FYLKENUMMER = [
+    {
+        navn: 'Møre og Romsdal',
+        fylkesnummer: '15',
+    },
+    {
+        navn: 'Telemark',
+        fylkesnummer: '40',
+    },
+    {
+        navn: 'Innlandet',
+        fylkesnummer: '34',
+    },
+    {
+        navn: 'Vestland',
+        fylkesnummer: '46',
+    },
+    {
+        navn: 'Rogaland',
+        fylkesnummer: '11',
+    },
+    {
+        navn: 'Trøndelag',
+        fylkesnummer: '50',
+    },
+    {
+        navn: 'Vestfold',
+        fylkesnummer: '39',
+    },
+    {
+        navn: 'Buskerud',
+        fylkesnummer: '33',
+    },
+    {
+        navn: 'Nordland',
+        fylkesnummer: '18',
+    },
+    {
+        navn: 'Troms',
+        fylkesnummer: '55',
+    },
+    {
+        navn: 'Finnmark',
+        fylkesnummer: '56',
+    },
+    {
+        navn: 'Oslo',
+        fylkesnummer: '03',
+    },
+    {
+        navn: 'Agder',
+        fylkesnummer: '42',
+    },
+    {
+        navn: 'Akershus',
+        fylkesnummer: '32',
+    },
+    {
+        navn: 'Østfold',
+        fylkesnummer: '31',
+    },
+];
