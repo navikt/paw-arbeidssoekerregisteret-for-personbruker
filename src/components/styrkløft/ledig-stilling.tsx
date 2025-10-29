de8a1cc58c265efb00e3674b72730bf4664caeb1
Geir Gåsodden <geir.gasodden@gmail.com>
2025-10-29 07:23:47 +0100

    r la inn mulighet for å kopiere lenke til den ledige stillingen

--- src/components/styrkløft/ledig-stilling.tsx
+++ src/components/styrkløft/ledig-stilling.tsx
@@ -1,5 +1,5 @@
-import { BodyShort, Box, Heading, HStack, VStack } from '@navikt/ds-react';
-import { Buildings3Icon, LocationPinIcon } from '@navikt/aksel-icons';
+import { BodyShort, Box, CopyButton, Heading, HStack, VStack } from '@navikt/ds-react';
+import { Buildings3Icon, LocationPinIcon, FilesIcon, CheckmarkIcon } from '@navikt/aksel-icons';
 
 interface Props {
     ledigStilling: any;
@@ -7,12 +7,12 @@ interface Props {
 
 function LedigStilling(props: Props) {
     const { ledigStilling } = props;
+    const ledigStillingUrl = `https://arbeidsplassen.nav.no/stillinger/stilling/${ledigStilling.uuid}`;
+
     return (
         <Box padding="space-16" borderRadius="large" shadow="xsmall">
             <Heading level={'2'} size={'small'}>
-                <a href={`https://arbeidsplassen.nav.no/stillinger/stilling/${ledigStilling.uuid}`}>
-                    {ledigStilling.tittel}
-                </a>
+                <a href={ledigStillingUrl}>{ledigStilling.tittel}</a>
             </Heading>
             <BodyShort weight="semibold" className={'mt-2 mb-4'}>
                 {ledigStilling.stillingbeskrivelse}
@@ -27,9 +27,16 @@ function LedigStilling(props: Props) {
                     <BodyShort>{ledigStilling.kommune}</BodyShort>
                 </HStack>
             </VStack>
-            <BodyShort weight="semibold" textColor="subtle" className={'mt-4'}>
-                {ledigStilling.soeknadsfrist?.raw}
-            </BodyShort>
+            <Box className="flex justify-between">
+                <BodyShort weight="semibold" textColor="subtle" className={'mt-4'}>
+                    {ledigStilling.soeknadsfrist?.raw}
+                </BodyShort>
+                <CopyButton
+                    copyText={ledigStillingUrl}
+                    icon={<FilesIcon title="Kopier lenke til den ledige stillingen" />}
+                    activeIcon={<CheckmarkIcon title="Kopierte lenke til stillingen" />}
+                />
+            </Box>
         </Box>
     );
 }
