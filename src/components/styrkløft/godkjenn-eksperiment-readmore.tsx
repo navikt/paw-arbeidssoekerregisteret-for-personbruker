c8ab23b3e12663ae988582b68f18ca0434031242
Christian Rennemo <christian.rennemo@nav.no>
2025-11-03 08:57:12 +0100

    lagt til nb og en på språk

--- src/components/styrkløft/godkjenn-eksperiment-readmore.tsx
+++ src/components/styrkløft/godkjenn-eksperiment-readmore.tsx
@@ -16,6 +16,26 @@ const TEKSTER = {
         adressebeskyttelse: 'Du kan ikke benytte tjenesten ennå dersom du har en form for adressebeskyttelse',
         testing: 'Denne tjenesten er under utprøving og kan forsvinne i fremtiden',
     },
+    nn: {
+        header: 'Meir informasjon',
+        om: 'Du kan setja opp eit forenkla søk mot arbeidsplassen.no og få vist resultatet her',
+        endre: 'Du kan endra søkjekriteria i ettertid',
+        privat: 'Det er berre du som ser kva søk du har lagra og kva stillingar du får opp',
+        avslutte: 'Du kan når som helst slå av søket',
+        sletting: 'Det lagra søket blir sletta etter 30 dagar når du ikkje lenger er registrert arbeidssøkjar',
+        adressebeskyttelse: 'Du kan ikkje nytta tenesta enno dersom du har ei form for adressevern',
+        testing: 'Denne tenesta er under utprøving og kan forsvinna i framtida',
+    },
+    en: {
+        header: 'More information',
+        om: 'You can set up a simplified search on arbeidsplassen.no and see the results here',
+        endre: 'You can change the search criteria later',
+        privat: 'Only you can see what search you have saved and what jobs you get',
+        avslutte: 'You can turn off the search at any time',
+        sletting: 'The saved search is deleted after 30 days when you are no longer registered as an employment seeker',
+        adressebeskyttelse: 'You cannot use the service yet if you have a form of address protection',
+        testing: 'This service is under testing and may disappear in the future',
+    },
 };
 
 export default function ReadMoreEksperiment(props: ReadMoreProps) {
