755ad39da413f43c86b12ce7511c159fae577df6
Jostein Holje <jostein.holje@nav.no>
2025-11-17 14:27:15 +0100

    Flytt styling til <style>

--- src/components/skyra/inline-skyra-survey.tsx
+++ src/components/skyra/inline-skyra-survey.tsx
@@ -8,20 +8,27 @@ export default function InlineSkyraSurvey(props: Props) {
     if (!isConsentingToSurveys()) return null;
 
     return (
-        // @ts-expect-error
-        <skyra-survey
-            slug="arbeids-og-velferdsetaten-nav/test-undersokelse"
-            style={{
-                border: '1px solid rgba(7, 26, 54, .21)',
-                padding: '0.5rem 1rem',
-                boxShadow: '0px 1px 3px 0px rgba(0, 0, 0, .15), 0px 0px 1px 0px rgba(0, 0, 0, .2)',
-                borderRadius: '8px',
-                '--skyra-bg-color': '#fff',
-                '--skyra-text-color': '#000',
-                '--skyra-action-text-color': '#fff',
-                '--skyra-action-color': 'rgba(0, 103, 197, 1)',
-            }}
-            // @ts-expect-error
-        ></skyra-survey>
+        <>
+            <style jsx>{`
+                skyra-survey[slug='arbeids-og-velferdsetaten-nav/test-undersokelse'] {
+                    box-shadow:
+                        0px 1px 3px 0px rgba(0, 0, 0, 0.15),
+                        0px 0px 1px 0px rgba(0, 0, 0, 0.2);
+                    border-radius: 8px;
+                    --skyra-bg-color: #fff;
+                    --skyra-text-color: #000;
+                    --skyra-action-text-color: #fff;
+                    --skyra-action-color: rgba(0, 103, 197, 1);
+                }
+                &::part(wrapper) {
+                    padding: 0.5rem 1rem;
+                }
+            `}</style>
+            {/* @ts-expect-error */}
+            <skyra-survey
+                slug="arbeids-og-velferdsetaten-nav/test-undersokelse"
+                // @ts-expect-error
+            ></skyra-survey>
+        </>
     );
 }
