02a574199e8bbf1d7cb85ed97e4017ab6ac199c9
Jostein Holje <jostein.holje@nav.no>
2025-12-10 13:00:18 +0100

    Viser nyhets/tips-boks hvis ikke underkategorier er valgt

---
+++ src/components/styrkløft/NyhetUnderkategoriAlert.tsx
@@ -0,0 +1,33 @@
+import { lagHentTekstForSprak, Sprak } from '@navikt/arbeidssokerregisteret-utils';
+import { LocalAlert } from '@navikt/ds-react';
+
+interface Props {
+    sprak: Sprak;
+}
+
+const TEKSTER = {
+    nb: {
+        title: 'Nyhet: nå kan du velge blant flere yrkeskategorier',
+        content: 'Endre søket ditt ved å trykke på ikonet ovenfor i det høyre hjørnet.',
+    },
+    nn: {
+        title: 'Nyheit: no kan du velja blant fleire yrkeskategoriar',
+        content: 'Endre søket ditt ved å trykkja på ikonet ovanfor i det høgre hjørnet.',
+    },
+    en: {
+        title: 'New feature: choose from more job categories',
+        content: 'Edit your search by clicking the icon above in the right corner.',
+    },
+};
+
+export default function NyhetUnderkategoriAlert(props: Props) {
+    const tekst = lagHentTekstForSprak(TEKSTER, props.sprak);
+    return (
+        <LocalAlert status="announcement" className={'mb-4'}>
+            <LocalAlert.Header>
+                <LocalAlert.Title>{tekst('title')}</LocalAlert.Title>
+            </LocalAlert.Header>
+            <LocalAlert.Content>{tekst('content')}</LocalAlert.Content>
+        </LocalAlert>
+    );
+}
