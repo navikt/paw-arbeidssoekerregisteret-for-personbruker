bb6f2efa586f41beeba7b30a9920eb379bb25d06
Jostein Holje <jostein.holje@nav.no>
2026-03-05 15:08:03 +0100

    Fjerner ubrukte filer

--- src/lib/date-utils.ts
+++ src/lib/date-utils.ts
@@ -59,22 +59,4 @@ function formaterDato(
 ): string {
     return dateString ? new Intl.DateTimeFormat('nb', options).format(new Date(dateString)) : 'Ingen tidspunkt funnet';
 }
-
-function prettyPrintDatoOgKlokkeslett(dato: string, locale?: Sprak) {
-    const now = new Date();
-    const date = new Date(dato);
-    const thisYear = now.getFullYear();
-    const year = date.getFullYear();
-    const timer = date.getHours();
-    const minutter = date.getMinutes();
-    const valgtSprak = !locale ? 'nb' : (locale as Sprak);
-    const month = monthNames[valgtSprak][date.getMonth()];
-
-    if (valgtSprak === 'en') {
-        return `${month} ${date.getDate()}.${thisYear !== year ? ' ' + year : ''} at ${timer.toString().length === 1 ? '0' : ''}${timer}:${minutter.toString().length === 1 ? '0' : ''}${minutter}`;
-    }
-
-    return `${date.getDate()}. ${month}${thisYear !== year ? ' ' + year : ''} klokken ${timer.toString().length === 1 ? '0' : ''}${timer}:${minutter.toString().length === 1 ? '0' : ''}${minutter}`;
-}
-
-export { prettyPrintDato, prettyPrintDatoOgKlokkeslett, formaterDato };
+export { prettyPrintDato, formaterDato };
