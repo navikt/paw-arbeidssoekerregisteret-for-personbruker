02bbc61ba9f5fec7a7c8bec183599bf8de5d7b1d
Jostein Holje <jostein.holje@nav.no>
2026-04-29 13:36:45 +0200

    Legger til story for avmeldt styrkeløfter på bekreftelse-kvittering

--- src/components/bekreftelse/kvittering.stories.tsx
+++ src/components/bekreftelse/kvittering.stories.tsx
@@ -143,3 +143,64 @@ export const KvitterinMedStillingerKomponent: Story = {
         },
     },
 };
+
+export const KvitterinMedAvmeldtStillinger: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/brukerprofil/ledigestillinger`, () => {
+                    return HttpResponse.json({
+                        soek: {
+                            fylker: [
+                                {
+                                    navn: 'Buskerud',
+                                    kommuner: [
+                                        {
+                                            navn: 'Bergen',
+                                            kommunenummer: '4601',
+                                        },
+                                    ],
+                                    fylkesnummer: '46',
+                                },
+                            ],
+                            styrk08: ['2114'],
+                        },
+                        resultat: [
+                            {
+                                arbeidsplassenNoId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+                                tittel: 'Nittedal Tannlegesenter har ledig stilling som tannhelsesekretær/tannlegeassistent, er du den rette?',
+                                stillingbeskrivelse: 'Tannlegeassistent, Tannhelsesekretær, Klinikkassistent',
+                                selskap: 'Nittedal Tannlegesenter Og Tannlegevakt',
+                                kommune: 'Nittedal, Norge, Oslo, Akershus',
+                                soeknadsfrist: {
+                                    raw: 'Søk senest torsdag 6. november',
+                                },
+                            },
+                            {
+                                arbeidsplassenNoId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+                                tittel: 'Do you want to be part of developing WorldHotels and taking the brand to new heights in Scandinavia?',
+                                stillingbeskrivelse: 'Head of Performance WorldHotels',
+                                selskap: 'BWH Hotels Scandinavia',
+                                kommune: 'Oslo',
+                                soeknadsfrist: {
+                                    raw: 'Søk snarest mulig',
+                                },
+                            },
+                        ],
+                    });
+                }),
+            ],
+        },
+    },
+    args: {
+        sprak: 'nb',
+        erUtmeldt: false,
+        harFlereBekreftelser: false,
+        onClick: () => console.log('onClick'),
+        brukerprofil: {
+            identitetsnummer: '42',
+            tjenestestatus: 'OPT_OUT',
+            stillingssoek: [],
+        },
+    },
+};
