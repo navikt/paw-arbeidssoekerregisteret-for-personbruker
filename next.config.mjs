c0d72fc503eddf3993a6a2a17d8bcc006cd0ccd1
Geir Gåsodden <geir.gasodden@gmail.com>
2026-08-20 07:24:38 +0200

    build: legger til sourcemap i Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    productionBrowserSourceMaps: true,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
 };
